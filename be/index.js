41a03772f5235d2657015e0e3d53f895b634ca37
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-02-23 11:37:01 +0800

    edit done

--- be/index.js
+++ be/index.js
@@ -54,7 +54,13 @@ app.post("/delete-user", (req, res) => {
 app.post("/edit-user", (req, res) => {
   let editedDummy = dummyData.users.map((user) => {
     if (user.id == req.body.id) {
-      return user.id != req.body.id;
+      // console.log("req.body: ", req.body);
+      // console.log("req.body.name: ", req.body.name);
+      // console.log("req.body.id: ", req.body.id);
+      // console.log("consoling in edit end point USER: ", user);
+      return { id: user.id, userName: req.body.userName, age: user.age };
+    } else {
+      return { id: user.id, userName: user.userName, age: user.age };
     }
   });
 
@@ -64,7 +70,7 @@ app.post("/edit-user", (req, res) => {
       res.send("Error happened when write file");
     } else {
       console.log("dummyData: ", dummyData);
-      res.send("dummyData changed success");
+      res.send("dummyData changed successfully");
     }
   });
 });
